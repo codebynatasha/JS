e194969432201bd55f1cd4e08185ad3be93026d8
Natasha <natashagul247@gmail.com>
2025-11-19 12:39:14 +0000

    Class 2 & 3 | Varibles, Data type and ECMA Standard

--- JS_Basic/index.js
+++ JS_Basic/index.js
@@ -1,1 +1,17 @@
-console.log ('Natasha')
+console.log ('Natasha') // First Lesson 
+const accountId = 12345 // Value can't be changed
+let accountEmail = 'natasha@gmail.com' // Block scope
+var accountPassword = '1122' // Block scope? No (Now, Not recommend)
+accountCity = 'unknown' // Don't do this at all
+// let accountState; //undefined
+// let accountState = null
+
+//accountId = 2 / Not allowed
+
+accountEmail = '67@gmail.com'
+accountPassword = '3344'
+accountCity = 'Rather not to say'
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); // Multiples prints at once
+
+//node JS_Basic/index.js
